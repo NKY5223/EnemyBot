cd6f10908c5f33147af7d8512f2687689c104f60
NKY5223 <76890154+NKY5223@users.noreply.github.com>
2021-07-31 09:28:15 +0800

    addded color to -p

--- commands/ping.js
+++ commands/ping.js
@@ -2,10 +2,14 @@ const Discord = require("discord.js");
 
 /** @type {import("../index").CommandFunc} */
 module.exports = message => {
-    message.channel.send("🏓 Pinging...").then(ping => {
+    message.channel.send(new Discord.MessageEmbed()
+        .setTitle("🏓 Ping...")
+        .setColor("#E82727")
+    ).then(ping => {
         ping.edit(new Discord.MessageEmbed()
             .setTitle("🏓 Pong!")
-            .setDescription(`Latency: ${ping.createdTimestamp - message.createdTimestamp}\nAPI Latency: ${message.client.ws.ping}`)
+            .setDescription(`Latency: ${ping.createdTimestamp - message.createdTimestamp}ms\nAPI Latency: ${message.client.ws.ping}ms`)
+            .setColor("#E82727")
         );
     });
 }
